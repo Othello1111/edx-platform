0d40bdc9485b22ca1d1543efa23ed9cb4cdf5368
Kyle McCormick <kmccormick@edx.org>
2019-08-30 13:20:03 -0400

    wip: remove enrollments.py for now

--- lms/djangoapps/program_enrollments/api/__init__.py
+++ lms/djangoapps/program_enrollments/api/__init__.py
@@ -1,7 +1,7 @@
 """
 Python API exposed by the program_enrollments app to other in-process apps.
 
-There are separate modules (enrollments.py, grades.py) for code-organization,
+There are separate modules (e.g., sturcture.py, grades.py) for code-organization,
 but we wildcard-import them all into here so that a client of this app
 can simply import from `lms.djangoapps.program_enrollments.api`.
 """
@@ -10,6 +10,6 @@ can simply import from `lms.djangoapps.program_enrollments.api`.
 from __future__ import absolute_import
 
 from ..constants import *
-from .enrollments import *
+# from .enrollments import * TODO replace this when enrollments.py is written
 from .grades import *
 from .structure import *
