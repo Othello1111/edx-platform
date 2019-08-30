44b8bd88f65dc543ef904c997b26bb5c09d65d1c
Kyle McCormick <kmccormick@edx.org>
2019-08-30 10:59:17 -0400

    Add ReadOnlyObjectsMixin in util.query
    Adds a mixin that can be put on model classes,
    providing an objects_readonly() classmethod that
    uses the read replica if it is available.

--- common/djangoapps/util/query.py
+++ common/djangoapps/util/query.py
@@ -6,6 +6,30 @@ from django.conf import settings
 
 def use_read_replica_if_available(queryset):
     """
-    If there is a database called 'read_replica', use that database for the queryset.
+    If there is a database called 'read_replica',
+    use that database for the queryset / manager.
     """
     return queryset.using("read_replica") if "read_replica" in settings.DATABASES else queryset
+
+
+class ReadOnlyObjectsMixin(object):
+    """
+    Defines a utility classmethod `objects_readonly`,
+    allowing querying like:
+        ProgramEnrollments.objects_readonly().filter(...)
+    as opposed to:
+        use_read_replica_if_available(ProgramEnrollments.objects.filter(...))
+
+    This is purely a wrapper around `use_read_replica_if_available`
+    for the purpose of syntactic convenience.
+
+    Assumes that subclass is a model with a manager named `objects`.
+    """
+
+    @classmethod
+    def objects_readonly(cls):
+        """
+        Returns the the default queryset of `cls.objects`, using the
+        read replica if it is available, and the default database if not.
+        """
+        return use_read_replica_if_available(cls.objects.all())
