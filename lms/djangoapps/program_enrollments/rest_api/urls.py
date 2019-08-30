bba24f81bd774c7e50b2f429593f65abbebfcce7
Kyle McCormick <kmccormick@edx.org>
2019-08-30 11:49:08 -0400

    wip: fix urls files; use relative app_name

--- lms/djangoapps/program_enrollments/rest_api/urls.py
+++ lms/djangoapps/program_enrollments/rest_api/urls.py
@@ -11,5 +11,5 @@ from .v1 import urls as v1_urls
 app_name = 'lms.djangoapps.program_enrollments'
 
 urlpatterns = [
-    url(r'^v1/', include(v1_urls, namespace='v1'))
+    url(r'^v1/', include(v1_urls))
 ]
