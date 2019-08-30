ee88e67dc4f89bc89ce571732ef5247099ff6b9d
Kyle McCormick <kmccormick@edx.org>
2019-08-30 15:40:50 -0400

    wip: remove unnecessary ReadOnlyObjectsMixin

--- common/djangoapps/util/query.py
+++ common/djangoapps/util/query.py
@@ -10,26 +10,3 @@ def use_read_replica_if_available(queryset):
     use that database for the queryset / manager.
     """
     return queryset.using("read_replica") if "read_replica" in settings.DATABASES else queryset
-
-
-class ReadOnlyObjectsMixin(object):
-    """
-    Defines a utility classmethod `objects_readonly`,
-    allowing querying like:
-        ProgramEnrollments.objects_readonly().filter(...)
-    as opposed to:
-        use_read_replica_if_available(ProgramEnrollments.objects.filter(...))
-
-    This is purely a wrapper around `use_read_replica_if_available`
-    for the purpose of syntactic convenience.
-
-    Assumes that subclass is a model with a manager named `objects`.
-    """
-
-    @classmethod
-    def objects_readonly(cls):
-        """
-        Returns the the default queryset of `cls.objects`, using the
-        read replica if it is available, and the default database if not.
-        """
-        return use_read_replica_if_available(cls.objects.all())
